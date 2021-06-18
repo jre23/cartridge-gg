ac76f750b6da2df412455de67103a3580f30be98
jre23 <jre.estrada@gmail.com>
2021-06-18 16:13:31 -0700

    fixed display flex issues

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -6,22 +6,22 @@ import "../../styles/Home.css";
 const Home = () => {
   return (
     <>
-      <div className="bg-dark text-white d-flex w-100  h-100 text-center">
-        <main className="my-auto w-100">
+      <main className="mainContainer bg-dark text-white text-center">
+        <div className="my-auto">
           <h1 className="mb-4">Cartridge GG</h1>
-          <h4 className="w-75 mx-auto mb-4">
+          <p className="w-75 mx-auto mb-4 mw-100 homeFont">
             A hub for games! This is a site for simple games I've created. Take
             a look at the Games page to play! Check the About page for my
             contact info, general inquiries, and the motivation behind this
             page!
-          </h4>
+          </p>
           <Link to="/games">
             <button className="btn btn-outline-light mt-3" type="button">
               GAMES
             </button>
           </Link>
-        </main>
-      </div>
+        </div>
+      </main>
     </>
   );
 };
