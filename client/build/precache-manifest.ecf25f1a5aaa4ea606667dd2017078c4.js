d81f8cb68cc2b743ccbbd5a88ce1901b43eb3a25
jre23 <jre.estrada@gmail.com>
2021-06-20 14:55:21 -0700

    npm run build

---
+++ client/build/precache-manifest.ecf25f1a5aaa4ea606667dd2017078c4.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3adb431f0d1cd26037efe27e1d56a6b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c53a7000c4e2ad2a2f4f",
+    "url": "/static/css/2.38e2dc45.chunk.css"
+  },
+  {
+    "revision": "f458b31ee1bcfa456eed",
+    "url": "/static/css/main.b63129f9.chunk.css"
+  },
+  {
+    "revision": "c53a7000c4e2ad2a2f4f",
+    "url": "/static/js/2.a7e8eaff.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.a7e8eaff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f458b31ee1bcfa456eed",
+    "url": "/static/js/main.98393049.chunk.js"
+  },
+  {
+    "revision": "32f714a93c2b72567c9d",
+    "url": "/static/js/runtime-main.79068382.js"
+  },
+  {
+    "revision": "b8e8573c7a2f25d4778b3340632e49c5",
+    "url": "/static/media/scratch_catNmouse.b8e8573c.png"
+  },
+  {
+    "revision": "d2d0a55378884ae86eb0ab88c3192cb3",
+    "url": "/static/media/scratch_cheesyPuffs.d2d0a553.png"
+  },
+  {
+    "revision": "820a4ee90d44adc28236773d8b506aa7",
+    "url": "/static/media/unsplash_alexeySavchenko_ps4controller.820a4ee9.jpg"
+  },
+  {
+    "revision": "c979cf9eba97b9a225615d3eff94a67f",
+    "url": "/static/media/unsplash_antonDarius_scissors.c979cf9e.jpg"
+  },
+  {
+    "revision": "de8890117272787c9ac3ba0ca57355e9",
+    "url": "/static/media/unsplash_cjtagupa_balabac.de889011.jpg"
+  },
+  {
+    "revision": "a3de79ef339d51150f16f0b5f31f97b9",
+    "url": "/static/media/unsplash_fadilahImani_rps.a3de79ef.jpg"
+  },
+  {
+    "revision": "ab333d06d49867c6cb47b85abd0682da",
+    "url": "/static/media/unsplash_kellySikkema_paper.ab333d06.jpg"
+  },
+  {
+    "revision": "160a71237c35453178eb3a19310de212",
+    "url": "/static/media/unsplash_mikaBaumeister_superMario.160a7123.jpg"
+  },
+  {
+    "revision": "6e571e9a04173c47d03c60788788e98c",
+    "url": "/static/media/unsplash_morShani_rock.6e571e9a.jpg"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cartridge-gg/precache-manifest.227cf1aae3b4d2e7f89588dd608308d6.js"
+  "/precache-manifest.ecf25f1a5aaa4ea606667dd2017078c4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cartridge-gg/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
