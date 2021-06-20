b68f1074707bc691a41f37dc3bbb478263fa2848
jre23 <jre.estrada@gmail.com>
2021-06-20 13:13:11 -0700

    npm run build

---
+++ client/build/precache-manifest.a7ea998912a517abfc70f9d8bb9bad0f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbb9f6cf1da40a929ef8f1c0ad801b69",
+    "url": "/cartridge-gg/index.html"
+  },
+  {
+    "revision": "267f37e3b49080877741",
+    "url": "/cartridge-gg/static/css/2.38e2dc45.chunk.css"
+  },
+  {
+    "revision": "71a480b9cd9e607f6c6e",
+    "url": "/cartridge-gg/static/css/main.81a0e183.chunk.css"
+  },
+  {
+    "revision": "267f37e3b49080877741",
+    "url": "/cartridge-gg/static/js/2.68b90c85.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/cartridge-gg/static/js/2.68b90c85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71a480b9cd9e607f6c6e",
+    "url": "/cartridge-gg/static/js/main.4b2e6637.chunk.js"
+  },
+  {
+    "revision": "bc3988d4363548e468e6",
+    "url": "/cartridge-gg/static/js/runtime-main.8c9f65a9.js"
+  },
+  {
+    "revision": "b8e8573c7a2f25d4778b3340632e49c5",
+    "url": "/cartridge-gg/static/media/scratch_catNmouse.b8e8573c.png"
+  },
+  {
+    "revision": "d2d0a55378884ae86eb0ab88c3192cb3",
+    "url": "/cartridge-gg/static/media/scratch_cheesyPuffs.d2d0a553.png"
+  },
+  {
+    "revision": "820a4ee90d44adc28236773d8b506aa7",
+    "url": "/cartridge-gg/static/media/unsplash_alexeySavchenko_ps4controller.820a4ee9.jpg"
+  },
+  {
+    "revision": "c979cf9eba97b9a225615d3eff94a67f",
+    "url": "/cartridge-gg/static/media/unsplash_antonDarius_scissors.c979cf9e.jpg"
+  },
+  {
+    "revision": "de8890117272787c9ac3ba0ca57355e9",
+    "url": "/cartridge-gg/static/media/unsplash_cjtagupa_balabac.de889011.jpg"
+  },
+  {
+    "revision": "a3de79ef339d51150f16f0b5f31f97b9",
+    "url": "/cartridge-gg/static/media/unsplash_fadilahImani_rps.a3de79ef.jpg"
+  },
+  {
+    "revision": "ab333d06d49867c6cb47b85abd0682da",
+    "url": "/cartridge-gg/static/media/unsplash_kellySikkema_paper.ab333d06.jpg"
+  },
+  {
+    "revision": "160a71237c35453178eb3a19310de212",
+    "url": "/cartridge-gg/static/media/unsplash_mikaBaumeister_superMario.160a7123.jpg"
+  },
+  {
+    "revision": "6e571e9a04173c47d03c60788788e98c",
+    "url": "/cartridge-gg/static/media/unsplash_morShani_rock.6e571e9a.jpg"
+  }
+]);
